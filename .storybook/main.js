dbd01835d913ee913ae2bfbdfff6b6d87b0a0bc1
Re-taro <aminokacho@gmail.com>
2022-04-14 01:49:59 +0900

    🔧 Storybookの設定を変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ['../src/components/**/**/*.stories.@(ts|tsx)'],
   addons: [
@@ -20,5 +23,19 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "@emotion/styled": toPath("node_modules/@emotion/styled"),
+          "emotion-theming": toPath("node_modules/@emotion/react")
+        }
+      }
+    };
   }
 }
