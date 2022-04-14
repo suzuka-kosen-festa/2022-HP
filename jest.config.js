bb77a79fc8c4ee430ceadfcbfd1598144ad6643b
Re-taro <aminokacho@gmail.com>
2022-04-14 22:35:23 +0900

    🐛 fix jest bugs

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
