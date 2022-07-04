86f01bf598a12570cb12f8c7786fedfe2e79e503
Rintaro Itokawa <me@re-taro.dev>
2022-07-05 00:34:27 +0900

    ✅ add some test

---
+++ src/components/atoms/text/text.test.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import { Text } from '.'
+
+it("(components) Text snap shot", () => {
+  const tree = render(<Text>Test</Text>)
+  expect(tree).toMatchSnapshot()
+})
---
+++ src/tests/pages/index.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen, within } from '@testing-library/react'
+import Home from '../../pages'
+
+it("(pages) Home snap shot", () => {
+  const tree = render(<Home />)
+  expect(tree).toMatchSnapshot()
+})
+
+it("(pages) Home integration", () => {
+  render(<Home />)
+  const main = within(screen.getByRole("main"))
+  expect(main.getByText("Hello Next.js")).toBeInTheDocument()
+})
