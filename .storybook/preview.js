cd96c4c6e92ec47c1e91a6e83b9a1e23509f322f
Rintaro Itokawa <me@re-taro.dev>
2022-07-16 23:44:23 +0900

    ✨ Storybookの設定を変更した
    babelの設定を読み込むようにし、next/routeへの対応を行った

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
-export const parameters = {
+module.exports = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-  },
+  }
 };
