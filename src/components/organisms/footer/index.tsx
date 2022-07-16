505f50e4afdf0a39414debc0ea86129aee7083cd
sansai-SNCT <syunzo1115@icloud.com>
2022-07-17 01:39:14 +0900

    Update: 余白の追加 #60

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -3,7 +3,7 @@ import tw, { css } from "twin.macro";
 const Footer: React.FC = () => {
   return (
     <footer>
-      <p css={tw`text-white bg-black text-center `}>&copy; 第57回鈴鹿高専祭実行委員会</p>
+      <p css={tw`text-white bg-black text-center p-4`}>&copy; 第57回鈴鹿高専祭実行委員会</p>
     </footer>
   );
 };
