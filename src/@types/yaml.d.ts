3684674a5fd41b2bccb44729dc8518517a0ca625
Rintaro Itokawa <me@re-taro.dev>
2022-07-16 13:33:45 +0900

    🏷️ yamlパーサのparse関数の返り値をジェネリックにした

---
+++ src/@types/yaml.d.ts
@@ -0,0 +1,14 @@
+import type { ParseOptions, DocumentOptions, SchemaOptions, ToJSOptions } from "yaml";
+import type { Reviver } from "yaml/dist/doc/applyReviver";
+import type { RField } from '../models/type'
+
+declare module "yaml" {
+  // eslint-disable-next-line unicorn/prevent-abbreviations
+  function parse<T extends RField>(src: string, options?: ParseOptions & DocumentOptions & SchemaOptions & ToJSOptions): T;
+  function parse<T extends RField>(
+    // eslint-disable-next-line unicorn/prevent-abbreviations
+    src: string,
+    reviver: Reviver,
+    options?: ParseOptions & DocumentOptions & SchemaOptions & ToJSOptions,
+  ): T;
+}
