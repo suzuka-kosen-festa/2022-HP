dfb700a802851a19892b781647af98f0f64ff5f0
sansai-SNCT <syunzo1115@icloud.com>
2022-07-17 01:38:34 +0900

    Update: 不要だったので外した

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,9 +1,9 @@
 import tw, { css } from "twin.macro";
-import { Text } from "../../atoms/text";
+
 const Footer: React.FC = () => {
   return (
     <footer>
-      <Text css={tw`text-gray-500`}>&copy; 第57回鈴鹿高専祭実行委員会</Text>
+      <p css={tw`text-white bg-black text-center `}>&copy; 第57回鈴鹿高専祭実行委員会</p>
     </footer>
   );
 };
