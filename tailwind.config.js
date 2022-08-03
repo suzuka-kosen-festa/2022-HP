c608bf61bf8780edbd0467c00a25816c145d4ee7
Rintaro Itokawa <me@re-taro.dev>
2022-08-03 18:11:43 +0900

    ✨ Tailwindに初期サイトのトークンを読み込ませる
    extendsしているので名前がかぶるとコチラが優先される

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const variables = require("./src/styles/_variables.json");
+
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
   darkMode: "class",
   plugins: [],
   content: ["./src/**/*.{ts,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        ...variables.global.color,
+      },
+    },
+  },
   variants: {
     extend: {},
   },
