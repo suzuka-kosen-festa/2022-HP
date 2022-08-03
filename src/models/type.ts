bf9b1a29e6b3fba6e0d40ed9ffa6585754dcd72f
Rintaro Itokawa <me@re-taro.dev>
2022-08-03 18:10:43 +0900

    🏷️ ButtonPropertiesの追加
    boxStyles: button自身のスタイル
    textStyles: 内部のテキストに対するスタイル
    ref: refは今回なし
    as: as={'a'}などで型をオーバーライド

--- src/models/type.ts
+++ src/models/type.ts
@@ -1,3 +1,5 @@
+import type { CSSInterpolation } from "@emotion/serialize";
+import React from "react";
 import { TEST, SPONSORS } from "./constant";
 
 export type Field<T extends RField> = {
@@ -15,3 +17,12 @@ export type RField = Sponsors | _Test;
 export type Sponsors = typeof SPONSORS;
 
 export type _Test = typeof TEST;
+
+export type ButtonProperties<T extends AnyComponent> = React.PropsWithChildren<
+  PropertiesOf<T> & {
+    boxStyles?: CSSInterpolation;
+    textStyles?: CSSInterpolation;
+    ref?: never;
+    as?: T;
+  }
+>;
