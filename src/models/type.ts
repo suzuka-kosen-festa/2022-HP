1de20abfde9440127cc7517eff61520a2ab5fdbe
Rintaro Itokawa <me@re-taro.dev>
2022-08-05 17:32:34 +0900

    ♻️ 汚いAPIの実装を修正
    主に型周りを整備した

--- src/models/type.ts
+++ src/models/type.ts
@@ -1,17 +1,24 @@
-import { TEST, SPONSORS } from "./constant";
-
-export type Field<T extends RField> = {
-  __typename: T;
-  name: string;
+export type _Field = {
+  __typename: FieldsKey;
+  path: string;
 };
 
-export type RYaml<T extends RField> = {
+export type RYaml<T extends Fields> = {
   data: T;
   field: string;
 };
 
-export type RField = Sponsors | _Test;
+export type RDataByField = Array<RYaml<Sponsors> | RYaml<Tests>>
 
-export type Sponsors = typeof SPONSORS;
+export type Fields = Sponsors | Tests
 
-export type _Test = typeof TEST;
+export type FieldsKey = "Sponsors" | "Tests"
+
+export type Sponsors = Array<string>;
+
+type _Test = {
+  id: number
+  name: string
+}
+
+export type Tests = Array<_Test>;
