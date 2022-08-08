6df33902db41c9c7d683b51b61dc2f39016034e6
Rintaro Itokawa <me@re-taro.dev>
2022-08-09 00:53:41 +0900

    ✨ buttonコンポーネントを実装
    型定義が曖昧だがこうするしかなかった。他にいい方法があったらリファクタリングする

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import tw from "twin.macro";
-import { ButtonProperties } from "../../../models/type";
+import { ButtonProperties } from "../../../models";
+import { Text } from "../text"
 
-const ButtonBox = tw.button`bg-white hover:bg-button-hover flex flex-row items-start px-8 py-2 gap-2.5 rounded-8`;
+const ButtonBox = tw.button`bg-white hover:bg-button-hover flex flex-row items-start px-8 py-2 gap-2.5 rounded-[2rem]`;
 
 const Button = <T extends AnyComponent>({
   children,
   boxStyles,
   textStyles,
+  as,
   ...rest
 }: ButtonProperties<T>): JSX.Element => (
-  <ButtonBox css={boxStyles} {...rest}>
-
+  <ButtonBox css={boxStyles} as={as} {...rest}>
+    <Text css={[tw`text-xs text-black`, textStyles]}>{children}</Text>
   </ButtonBox>
-)
+);
+
+export { Button }
