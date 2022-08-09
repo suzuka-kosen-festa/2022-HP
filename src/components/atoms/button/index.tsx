42af59e70d76eec021d56aead2420f46d055abe0
Rintaro Itokawa <me@re-taro.dev>
2022-08-09 19:08:57 +0900

    🚑️ anchor buttonにした際などにUIが崩れるバグを修正
    inline blockにしてあげたら世界は救われる

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -3,7 +3,7 @@ import tw from "twin.macro";
 import { ButtonProperties } from "../../../models";
 import { Text } from "../text";
 
-const ButtonBox = tw.button`bg-white hover:bg-button-hover flex flex-row items-start px-8 py-2 gap-2.5 rounded-[2rem]`;
+const ButtonBox = tw.button`bg-white hover:bg-button-hover inline-block items-start px-8 py-2 gap-2.5 rounded-[2rem]`;
 
 const Button = <T extends AnyComponent>({
   children,
