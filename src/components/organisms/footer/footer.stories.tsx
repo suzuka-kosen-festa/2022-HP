5c40a9cb3c7f172bcc0104ce92c3179d55d99236
Shunta Yamagiwa <62200647+Sansai-snct@users.noreply.github.com>
2022-08-10 18:57:57 +0900

    Update: 不要なpropの削除
    Co-authored-by: Rintaro Itokawa <me@re-taro.dev>

--- src/components/organisms/footer/footer.stories.tsx
+++ src/components/organisms/footer/footer.stories.tsx
@@ -3,6 +3,6 @@ import { Footer } from ".";
 
 type Story = ComponentStoryObj<typeof Footer>;
 
-export default { args: { title: "Footer" }, component: Footer } as ComponentMeta<typeof Footer>;
+export default { component: Footer } as ComponentMeta<typeof Footer>;
 
 export const Default: Story = {};
