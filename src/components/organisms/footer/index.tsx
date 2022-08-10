49b3f165400f290ba010579365f2c9120c449814
sansai-SNCT <syunzo1115@icloud.com>
2022-08-10 14:29:59 +0900

    Update: 不要なreturnの削除#60

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,11 +1,9 @@
 import tw, { css } from "twin.macro";
 
-const Footer: React.FC = () => {
-  return (
-    <footer>
-      <p css={tw`text-white bg-black text-center p-4`}>&copy; 第57回鈴鹿高専祭実行委員会</p>
-    </footer>
-  );
-};
+const Footer: React.FC = () => (
+  <footer>
+    <p css={tw`text-white bg-black text-center p-4`}>&copy; 第57回鈴鹿高専祭実行委員会</p>
+  </footer>
+);
 
 export { Footer };
