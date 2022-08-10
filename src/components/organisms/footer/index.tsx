c45db69605de4b36463a269a1f2bd3198e443ba7
sansai-SNCT <syunzo1115@icloud.com>
2022-08-10 19:07:44 +0900

    Update: 位置固定を削除 #60

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,7 +1,7 @@
 import tw, { css } from "twin.macro";
 import { Text } from "../../atoms/text";
 const Footer: React.FC = () => (
-  <footer css={tw`absolute bottom-0 w-screen`}>
+  <footer css={tw` w-screen`}>
     <Text css={tw`bg-black text-center p-2`}>&copy; 第57回鈴鹿高専祭実行委員会</Text>
   </footer>
 );
