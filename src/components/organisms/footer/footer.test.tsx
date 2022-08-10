1ed1543cc1c855b4002f1a3e951da9ab72ab1bd3
sansai-SNCT <syunzo1115@icloud.com>
2022-08-10 19:07:05 +0900

    Update　テストの追加 #60

--- src/components/organisms/footer/footer.test.tsx
+++ src/components/organisms/footer/footer.test.tsx
@@ -6,8 +6,16 @@ import * as stories from "./footer.stories";
 const { Default } = composeStories(stories);
 
 describe("(components) organisms/footer", () => {
+  test("default footer should be organisms", () => {
+    const { container } = render(<Default />);
+    expect(container).toBeOrganisms();
+  });
   test("take snap shot", () => {
     const { container } = render(<Default />);
     expect(container).toMatchSnapshot();
+  });
+  test("to be [role=footer]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("footer")).toBeInTheDocument();
   });
 });
