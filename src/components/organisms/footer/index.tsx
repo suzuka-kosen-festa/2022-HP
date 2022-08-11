239076098d6a72b3b6ae373b4501514565b8ae26
sansai-SNCT <syunzo1115@icloud.com>
2022-08-12 01:40:17 +0900

    Update: roleを付与 #60

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import tw from "twin.macro";
 import { Text } from "../../atoms/text";
 const Footer: React.FC = () => (
-  <footer css={tw` w-screen`}>
-    <Text css={tw`bg-black text-center p-2`}>&copy; 第57回鈴鹿高専祭実行委員会</Text>
+  <footer role={"contentinfo"} css={tw` w-screen`}>
+    <Text role={"link"} as="a" css={tw`bg-black text-center p-2`}>
+      &copy; 第57回鈴鹿高専祭実行委員会
+    </Text>
   </footer>
 );
 
