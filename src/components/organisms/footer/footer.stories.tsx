42706ef82542a9a17f5bf438c79527b4c3b4914d
sansai-SNCT <syunzo1115@icloud.com>
2022-08-12 01:07:10 +0900

    Delete: atomsに移動する

--- src/components/organisms/footer/footer.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentStoryObj, ComponentMeta } from "@storybook/react";
-import { Footer } from ".";
-type T = typeof Footer;
-type Story = ComponentStoryObj<T>;
-
-export default { component: Footer } as ComponentMeta<T>;
-
-export const Default: Story = {};
