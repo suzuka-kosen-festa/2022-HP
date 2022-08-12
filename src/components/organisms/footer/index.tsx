e01b5b3b0c8f7ac72823a6eb78e0b14d871ec2c3
sansai-SNCT <syunzo1115@icloud.com>
2022-08-12 21:28:37 +0900

    Update: リンクテキストに変更 #60

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import tw from "twin.macro";
-import { Text } from "../../atoms/text";
+import { AnchorText } from "../../atoms/anchor-text";
+
 const Footer: React.FC = () => (
   <footer role={"contentinfo"} css={tw` w-screen`}>
-    <Text role={"link"} as="a" css={tw`bg-black text-center p-2`}>
+    <AnchorText
+      href="https://github.com/suzuka-kosen-festa/2022-HP/blob/main/LICENSE"
+      css={tw`bg-black text-center p-2`}
+    >
       &copy; 第57回鈴鹿高専祭実行委員会
-    </Text>
+    </AnchorText>
   </footer>
 );
 
