cef2a07110e8890d052b8023cb1a5e549c439785
Rintaro Itokawa <me@re-taro.dev>
2022-08-12 16:12:36 +0900

    anchor textコンポーネントを実装した (#125)
    * 🩹 testにhref属性の指定を忘れていたので指定した
    
    href="#"
    
    * ✨ anchor textコンポーネントを実装した
    
    textコンポーネントをselect可能にしたもの. p to a
    
    * ✨ Storybookを設定
    
    * ✅ testの設定
    
    * [skip ci] auto-fixable format by github-actions
    
    * ♻️ ディレクトリ名が分かりづらかったため命名し直した
    
    * 📸 snap shotの修正
    
    * 🚚 ファイルの命名規則を守っていなかったため修正
    
    何故気づかない？
    
    * 📸 snapshotを更新
    
    名前変更に伴うもの
    
    Co-authored-by: re-taro <re-taro@users.noreply.github.com>

---
+++ src/components/atoms/anchor-text/anchor-text.test.tsx
@@ -0,0 +1,23 @@
+import { composeStories } from "@storybook/testing-react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import * as stories from "./anchor-text.stories";
+
+const { Default } = composeStories(stories);
+
+const options = { name: "This is test" };
+
+describe("(components) atoms/anchor", () => {
+  test("to be atoms", () => {
+    const { container } = render(<Default />);
+    expect(container).toBeAtom();
+  });
+  test("to be [role=link]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("link", options)).toBeInTheDocument();
+  });
+  test("take snap shot", () => {
+    const { container } = render(<Default />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/atoms/anchor-text/index.tsx
@@ -0,0 +1,5 @@
+import tw from "twin.macro";
+
+const AnchorText = tw.a`font-zen text-white text-base`;
+
+export { AnchorText };
