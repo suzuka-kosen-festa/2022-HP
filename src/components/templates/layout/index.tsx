9fdc8b7cbf44303d2d27689b15c40126c61331cb
Rintaro Itokawa <me@re-taro.dev>
2022-08-12 23:15:42 +0900

    🚑️ propertiesをもたせるのを忘れていました
    **切腹**

--- src/components/templates/layout/index.tsx
+++ src/components/templates/layout/index.tsx
@@ -13,7 +13,9 @@ const Layout: React.FC<LayoutProperties> = ({ children, ...rest }) => (
   <LayoutBox {...rest}>
     <MainBox>
       <Main role={"main"}>{children}</Main>
-      <Footer />
+      <Footer link={"https://github.com/suzuka-kosen-festa/2022-HP/blob/main/LICENSE"}>
+        &copy; 第57回鈴鹿高専祭実行委員会
+      </Footer>
     </MainBox>
   </LayoutBox>
 );
