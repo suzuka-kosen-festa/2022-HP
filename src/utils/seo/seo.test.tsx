d3f859662b08961dbcef44b3bce358f10adcf8f3
Rintaro Itokawa <me@re-taro.dev>
2022-08-14 12:18:40 +0900

    ✅ headタグのsnapshotを作成するtest

---
+++ src/utils/seo/seo.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import "../../tests/mocks/head.mock"
+import { render } from "@testing-library/react";
+import { Seo } from "."
+
+describe("(utils) Seo test", () => {
+  test("take snap shot", () => {
+    render(<Seo title={"Re:ROAD | 第57回鈴鹿高専祭"} description={"まだ何色にも色づいていない第57回鈴鹿高専祭を今から色づけていきます。"} />)
+    expect(document.head).toMatchSnapshot()
+  })
+})
