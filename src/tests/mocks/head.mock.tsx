4331abef02e58982e54ef2c8b7520eb32048884a
Rintaro Itokawa <me@re-taro.dev>
2022-08-14 13:16:30 +0900

    🐛 fragmentを暗黙的に用いることを禁止する

--- src/tests/mocks/head.mock.tsx
+++ src/tests/mocks/head.mock.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const HeadMock: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <>{ReactDOM.createPortal(children, document.head)}</>
+  <React.Fragment>{ReactDOM.createPortal(children, document.head)}</React.Fragment>
 );
 
 jest.doMock("next/head", () => HeadMock);
