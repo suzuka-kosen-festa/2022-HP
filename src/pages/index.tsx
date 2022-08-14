413f0c6f5a5101802d21da1890582be4acb0d7dd
Rintaro Itokawa <me@re-taro.dev>
2022-08-14 12:18:52 +0900

    ✨ 導入してみる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
+import React from "react";
 import tw from "twin.macro";
 import { Text } from "../components/atoms/text";
+import {Seo} from "../utils/seo";
 
 const Home: NextPage = () => (
-  <main>
-    <Text css={tw`text-xl`}>Hello Next.js</Text>
-  </main>
+  <React.Fragment>
+    <Seo title={"Re:ROAD | 第57回鈴鹿高専祭"} description={"まだ何色にも色づいていない第57回鈴鹿高専祭を今から色づけていきます。"} />
+    <main>
+      <Text css={tw`text-xl`}>Hello Next.js</Text>
+    </main>
+  </React.Fragment>
 );
 
 export default Home;
