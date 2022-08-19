c82dd5fc1625d02d8e924de79c835f932c9b30cc
sansai-SNCT <syunzo1115@icloud.com>
2022-08-20 00:38:20 +0900

    Create: テストの追加

---
+++ src/components/organisms/about-section/about-section.test.tsx
@@ -0,0 +1,25 @@
+import { composeStories } from "@storybook/testing-react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import * as stories from "./about-section.stories";
+
+const { Default } = composeStories(stories);
+
+const regionoptions = {
+  name: "鈴鹿高専について",
+};
+
+describe("(components) organisms/about-section", () => {
+  test("to be organisms", () => {
+    const { container } = render(<Default />);
+    expect(container).toBeOrganism();
+  });
+  test("to be [role=region]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("region", regionoptions));
+  });
+  test("take snap shot", () => {
+    const { container } = render(<Default />);
+    expect(container).toMatchSnapshot();
+  });
+});
