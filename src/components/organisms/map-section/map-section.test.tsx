1d2e0150399eac7ffee832400e87e10cf6b52ab2
Rintaro Itokawa <me@re-taro.dev>
2022-08-19 17:21:51 +0900

    ⚡️ 整形

--- src/components/organisms/map-section/map-section.test.tsx
+++ src/components/organisms/map-section/map-section.test.tsx
@@ -1,13 +1,13 @@
 import { composeStories } from "@storybook/testing-react";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import * as stories from "./map-section.stories"
+import * as stories from "./map-section.stories";
 
 const { Default } = composeStories(stories);
 
 const regionoptions = {
-  name: "アクセス"
-}
+  name: "アクセス",
+};
 
 describe("(components) organisms/map-section", () => {
   test("to be organisms", () => {
@@ -15,9 +15,9 @@ describe("(components) organisms/map-section", () => {
     expect(container).toBeOrganism();
   });
   test("to be [role=region]", () => {
-    const { getByRole } = render(<Default />)
-    expect(getByRole("region", regionoptions))
-  })
+    const { getByRole } = render(<Default />);
+    expect(getByRole("region", regionoptions));
+  });
   test("take snap shot", () => {
     const { container } = render(<Default />);
     expect(container).toMatchSnapshot();
