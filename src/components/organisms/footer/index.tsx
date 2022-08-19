bea42f607da0fba97651d6eda38404ba94d30c3e
Rintaro Itokawa <me@re-taro.dev>
2022-08-19 22:48:48 +0900

    ✨ paddingの修正
    paddingは比率を合わせるため100vwをcalcで良い感じに調整してください。またpadding top bottomさえ指定されていたら良かったので変更しました

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -3,7 +3,7 @@ import tw from "twin.macro";
 import type { FooterProperties } from "../../../models";
 import { AnchorText } from "../../atoms/anchor-text";
 
-const FooterBox = tw.footer`bg-black text-center leading-[calc(8px + 16 * ((100vw - 378px) / 1134))] p-[calc(8px + 16 * ((100vw - 378px) / 1134))] w-screen`;
+const FooterBox = tw.footer`bg-black text-center leading-[calc(8px + 16 * ((100vw - 378px) / 1134))] py-[calc(100vw / 63)] w-screen`;
 
 const Footer: React.FC<FooterProperties> = ({ link, children, ...rest }) => (
   <FooterBox role={"contentinfo"} {...rest}>
