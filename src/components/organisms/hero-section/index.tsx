ea79e67e4498f760c4bb114f99d7e74cba5f6791
Rintaro Itokawa <me@re-taro.dev>
2022-08-20 03:31:28 +0900

    ✨ heroセクションの実装
    CSS芸をしているためレビューお願いします

---
+++ src/components/organisms/hero-section/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import tw, { css } from "twin.macro";
+import type { HeroSectionProperties } from "../../../models";
+import { Text } from "../../atoms/text";
+
+const HeroSectionContainer = tw.section`flex flex-col`;
+
+const Backgorund = tw.div`flex flex-col items-center justify-center w-full h-[calc(200vw / 3)] md:h-screen bg-center bg-cover`;
+
+const HeroSection: React.FC<HeroSectionProperties> = ({ date, title, ...rest }) => (
+  <HeroSectionContainer aria-label={"第57回鈴鹿高専祭"} {...rest}>
+    <Backgorund
+      css={css`
+        background-image: linear-gradient(rgba(34, 31, 31, 0.9), rgba(34, 31, 31, 0.9)), url("/statics/gogo.webp");
+      `}
+    >
+      <Text css={tw`text-[calc(12px + 36 * ((100vw - 378px) / 1134))]`}>{title}</Text>
+      <img src={"/statics/logo.webp"} alt={"第57回鈴鹿高専祭、Re:ROADのロゴ"} css={tw`w-[calc(8200vw / 189)]`} />
+      <Text css={tw`text-[calc(12px + 36 * ((100vw - 378px) / 1134))]`}>{date}</Text>
+    </Backgorund>
+  </HeroSectionContainer>
+);
+
+export { HeroSection };
