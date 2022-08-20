538f6a4a04b9a39822041135d858da22af830283
Rintaro Itokawa <me@re-taro.dev>
2022-08-20 13:22:34 +0900

    mobileの文字サイズを大きくした (#142)
    * ♻️ mobileの文字サイズを大きくした
    
    SEO的に12pxを切るのは好ましくない
    
    * [skip ci] auto-fixable format by github-actions
    
    * ⚡️ 整形
    
    Co-authored-by: re-taro <re-taro@users.noreply.github.com>

--- src/components/atoms/anchor-text/index.tsx
+++ src/components/atoms/anchor-text/index.tsx
@@ -1,5 +1,5 @@
 import tw from "twin.macro";
 
-const AnchorText = tw.a`font-zen text-white text-[calc(8px + 8 * ((100vw - 378px) / 1134))]`;
+const AnchorText = tw.a`font-zen text-white text-[calc(12px + 4 * ((100vw - 378px) / 1134))]`;
 
 export { AnchorText };
