2b4f126cd780e705331c5d36cf4c252923a8008b
sansai-SNCT <syunzo1115@icloud.com>
2022-08-20 13:48:57 +0900

    Fix: Buttonの愚かな改悪を修正

--- src/components/organisms/about-section/index.tsx
+++ src/components/organisms/about-section/index.tsx
@@ -1,24 +1,18 @@
 import React from "react";
+import { AnchorButton } from "src/components/atoms/button/button.stories";
 import tw from "twin.macro";
 import { AboutSectionProperties } from "../../../models";
 import { Button } from "../../atoms/button";
 import { Heading } from "../../atoms/heading";
 import { Text } from "../../atoms/text";
 
-const AboutSectionContainer = tw.section`flex flex-col space-y-[calc(200vw / 63)]`;
+const AboutSectionContainer = tw.section`flex-col  space-y-[calc(100vw / 63)]`;
 
 const AboutSection: React.FC<AboutSectionProperties> = ({ children, link, text, title }) => (
   <AboutSectionContainer aria-label={"鈴鹿高専について"}>
     <Heading>{title}</Heading>
     <Text>{children}</Text>
-    <Button
-      as="a"
-      href={link}
-      target={"_brank"}
-      rel={"noopener noreferrer"}
-      role={"link"}
-      css={[tw`w-[calc((100vw / 2)] text-center`]}
-    >
+    <Button as="a" href={link} target={"_brank"} rel={"noopener noreferrer"} role={"link"}>
       {text}
     </Button>
   </AboutSectionContainer>
