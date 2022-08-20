9827b1b2ba808cd2fb52f1cd9ad865eaecb8bc01
Rintaro Itokawa <me@re-taro.dev>
2022-08-20 11:42:17 +0900

    ⚗️ logoの縦横を決定してFCPを上げた

--- src/components/organisms/hero-section/index.tsx
+++ src/components/organisms/hero-section/index.tsx
@@ -5,7 +5,7 @@ import { Text } from "../../atoms/text";
 
 const HeroSectionContainer = tw.section`flex flex-col`;
 
-const Backgorund = tw.div`flex flex-col items-center justify-center w-full h-[calc(200vw / 3)] md:h-screen bg-center bg-cover`;
+const Backgorund = tw.div`flex flex-col items-center justify-center w-full h-[calc(200vw / 3)] lg:h-screen bg-center bg-cover`;
 
 const HeroSection: React.FC<HeroSectionProperties> = ({ date, title, ...rest }) => (
   <HeroSectionContainer aria-label={"第57回鈴鹿高専祭"} {...rest}>
@@ -15,7 +15,7 @@ const HeroSection: React.FC<HeroSectionProperties> = ({ date, title, ...rest }) 
       `}
     >
       <Text css={tw`text-[calc(12px + 36 * ((100vw - 378px) / 1134))]`}>{title}</Text>
-      <img src={"/statics/logo.webp"} alt={"第57回鈴鹿高専祭、Re:ROADのロゴ"} css={tw`w-[calc(8200vw / 189)]`} />
+      <img src={"/statics/logo.webp"} alt={"第57回鈴鹿高専祭、Re:ROADのロゴ"} width={1212} height={247} css={tw`w-[calc(8200vw / 189)]`} />
       <Text css={tw`text-[calc(12px + 36 * ((100vw - 378px) / 1134))]`}>{date}</Text>
     </Backgorund>
   </HeroSectionContainer>
