ea41415dbc2a317469f8976fcfc50642c4973f34
sansai-SNCT <syunzo1115@icloud.com>
2022-08-22 15:08:23 +0900

    Update: 企業一覧をリストで再実装

--- src/components/organisms/sponsor-section/index.tsx
+++ src/components/organisms/sponsor-section/index.tsx
@@ -10,7 +10,16 @@ const SponsorSection: React.FC<SponsorSectionProperties> = ({ children, sponsors
   <SponsorSectionContainar aria-label={"協賛企業"} {...rest}>
     <Heading>{title}</Heading>
     <Text css={tw`whitespace-pre-wrap`}>{children}</Text>
-    <Text css={tw`whitespace-pre-wrap`}>{sponsors}</Text>
+    <ul css={tw`whitespace-pre-wrap`}>
+      {sponsors.map((item, index) => (
+        <li
+          css={tw`list-disc list-inside font-zen text-white text-[calc(12px + 4 * ((100vw - 378px) / 1134))] select-none`}
+          key={index}
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
   </SponsorSectionContainar>
 );
 
