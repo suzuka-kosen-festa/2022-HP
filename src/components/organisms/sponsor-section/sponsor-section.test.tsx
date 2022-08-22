73484a86e1c70a141235a74533cd24f021f19bd4
sansai-SNCT <syunzo1115@icloud.com>
2022-08-22 16:55:38 +0900

    Update: テストの追加

--- src/components/organisms/sponsor-section/sponsor-section.test.tsx
+++ src/components/organisms/sponsor-section/sponsor-section.test.tsx
@@ -1,14 +1,17 @@
 import { composeStories } from "@storybook/testing-react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { getByRole, render } from "@testing-library/react";
 import * as stories from "./sponsor-section.stories";
 
 const { Default } = composeStories(stories);
 
-const options = {
+const sponsoroptions = {
   name: "協賛企業",
 };
 
+const listoptions = {
+  name: "",
+};
 describe("(components) organisms/sponsor-section", () => {
   test("to be organisms", () => {
     const { container } = render(<Default />);
@@ -16,10 +19,15 @@ describe("(components) organisms/sponsor-section", () => {
   });
   test("to be [role=region]", () => {
     const { getByRole } = render(<Default />);
-    expect(getByRole("region", options));
+    expect(getByRole("region", sponsoroptions));
+  });
+  test("to be [role=list]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("list", listoptions));
   });
   test("take snap shot", () => {
     const { container } = render(<Default />);
+    expect(getByRole);
     expect(container).toMatchSnapshot();
   });
 });
