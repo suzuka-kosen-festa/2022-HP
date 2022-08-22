2d3b20e181e0f45ab7f7074171b682dc24162081
sansai-SNCT <syunzo1115@icloud.com>
2022-08-22 16:47:00 +0900

    Update: リストの書き方を変更

--- src/components/organisms/sponsor-section/index.tsx
+++ src/components/organisms/sponsor-section/index.tsx
@@ -5,19 +5,15 @@ import { Heading } from "../../atoms/heading";
 import { Text } from "../../atoms/text";
 
 const SponsorSectionContainar = tw.section`space-y-[calc(200vw / 63)]`;
-
 const SponsorSection: React.FC<SponsorSectionProperties> = ({ children, sponsors, title, ...rest }) => (
   <SponsorSectionContainar aria-label={"協賛企業"} {...rest}>
     <Heading>{title}</Heading>
     <Text css={tw`whitespace-pre-wrap`}>{children}</Text>
     <ul css={tw`whitespace-pre-wrap`}>
-      {sponsors.map((item, index) => (
-        <li
-          css={tw`list-disc list-inside font-zen text-white text-[calc(12px + 4 * ((100vw - 378px) / 1134))] select-none`}
-          key={index}
-        >
+      {sponsors.map((item, key) => (
+        <Text as={"li"} css={tw`list-disc list-inside`} key={`${item}-${key}`}>
           {item}
-        </li>
+        </Text>
       ))}
     </ul>
   </SponsorSectionContainar>
