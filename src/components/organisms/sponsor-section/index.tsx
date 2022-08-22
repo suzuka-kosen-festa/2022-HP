38a8af4f6b6d15cda8e1a6de18c50609f97cf21e
sansai-SNCT <syunzo1115@icloud.com>
2022-08-22 16:50:12 +0900

    Update: 汚染を破壊

--- src/components/organisms/sponsor-section/index.tsx
+++ src/components/organisms/sponsor-section/index.tsx
@@ -5,17 +5,18 @@ import { Heading } from "../../atoms/heading";
 import { Text } from "../../atoms/text";
 
 const SponsorSectionContainar = tw.section`space-y-[calc(200vw / 63)]`;
+const SponsorList = tw.ul``;
 const SponsorSection: React.FC<SponsorSectionProperties> = ({ children, sponsors, title, ...rest }) => (
   <SponsorSectionContainar aria-label={"協賛企業"} {...rest}>
     <Heading>{title}</Heading>
     <Text css={tw`whitespace-pre-wrap`}>{children}</Text>
-    <ul css={tw`whitespace-pre-wrap`}>
+    <SponsorList>
       {sponsors.map((item, key) => (
         <Text as={"li"} css={tw`list-disc list-inside`} key={`${item}-${key}`}>
           {item}
         </Text>
       ))}
-    </ul>
+    </SponsorList>
   </SponsorSectionContainar>
 );
 
