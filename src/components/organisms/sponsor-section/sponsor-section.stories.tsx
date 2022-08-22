30096b1162d733fc27f25bfc639c64ddb1f5cf7c
sansai-SNCT <syunzo1115@icloud.com>
2022-08-22 15:07:50 +0900

    Update: sponsorsを配列に変更

--- src/components/organisms/sponsor-section/sponsor-section.stories.tsx
+++ src/components/organisms/sponsor-section/sponsor-section.stories.tsx
@@ -6,7 +6,7 @@ type Story = ComponentStoryObj<T>;
 
 const data = {
   children: "今年の高専祭は3年ぶりの開催。\n以下の企業様が鈴鹿高専祭を応援してくれています！",
-  sponsors: "・株式会社xx\n・...",
+  sponsors: ["株式会社xx", "..."],
   title: "協賛企業",
 };
 
