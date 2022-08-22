b5cfd86d908990525841e9c746472824cd585c1f
sansai-SNCT <syunzo1115@icloud.com>
2022-08-23 00:21:08 +0900

    Update: Headの中身を戻した

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,11 @@ class Document extends NextDocument {
   render(): ReactElement {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/seo/apple-touch-icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
