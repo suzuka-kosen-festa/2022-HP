849229a84b534b8fb20f194f4b40be6556db6242
Rintaro Itokawa <me@re-taro.dev>
2022-08-26 01:33:16 +0900

    Twitterコンポーネントのレスポンシブ化及びFCPの改善
    width: 300px以下のスマホへの動作保証はありません。

--- src/components/molecules/twitter/index.tsx
+++ src/components/molecules/twitter/index.tsx
@@ -1,4 +1,11 @@
 import React, { useEffect } from "react";
+import tw from "twin.macro";
+
+const TwitterContainer = tw.figure`w-[300px] h-[420px]`;
+
+const TwitterMB = tw.div`inline sm:hidden`;
+
+const TwitterPC = tw.div`hidden sm:inline`;
 
 const Twitter: React.FC = () => {
   useEffect(() => {
@@ -10,16 +17,28 @@ const Twitter: React.FC = () => {
     };
   }, []);
   return (
-    <figure>
-      <a
-        className={"twitter-timeline"}
-        data-width={"288"}
-        data-height={"410"}
-        href={"https://twitter.com/KOSENFESTA?ref_src=twsrc%5Etfw"}
-      >
-        Tweets by KOSENFESTA
-      </a>
-    </figure>
+    <TwitterContainer>
+      <TwitterMB>
+        <a
+          className={"twitter-timeline"}
+          data-width={"240"}
+          data-height={"420"}
+          href={"https://twitter.com/KOSENFESTA?ref_src=twsrc%5Etfw"}
+        >
+          Tweets by KOSENFESTA
+        </a>
+      </TwitterMB>
+      <TwitterPC>
+        <a
+          className={"twitter-timeline"}
+          data-width={"288"}
+          data-height={"410"}
+          href={"https://twitter.com/KOSENFESTA?ref_src=twsrc%5Etfw"}
+        >
+          Tweets by KOSENFESTA
+        </a>
+      </TwitterPC>
+    </TwitterContainer>
   );
 };
 
