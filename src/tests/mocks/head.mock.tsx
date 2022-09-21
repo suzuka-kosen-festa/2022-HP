7baa7688539464aeed3941272567d953bdad91a9
Rintaro Itokawa <me@re-taro.dev>
2022-09-21 23:06:25 +0900

    feat: 使用していないファイルなどを削除

--- src/tests/mocks/head.mock.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const HeadMock: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <React.Fragment>{ReactDOM.createPortal(children, document.head)}</React.Fragment>
-);
-
-jest.doMock("next/head", () => HeadMock);
