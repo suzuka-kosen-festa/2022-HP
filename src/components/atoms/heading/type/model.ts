4938d7d4990eeaf879fea59bf880bc1edd56c413
Rintaro Itokawa <me@re-taro.dev>
2022-09-24 03:25:49 +0900

    fix: Fix CI
    fix model to optional type

--- src/components/atoms/heading/type/model.ts
+++ src/components/atoms/heading/type/model.ts
@@ -3,5 +3,5 @@ import type { ComponentProps, ReactHTML } from "react";
 export type ColorVariant = "yellow" | "green" | "blue" | "purple" | "red";
 
 export type HeadingProperties = ComponentProps<ReactHTML["h1"]> & {
-  color: ColorVariant | undefined;
+  color?: ColorVariant;
 };
