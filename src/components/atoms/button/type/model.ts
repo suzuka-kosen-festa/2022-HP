1b6a59c8ac05f027cf518dee1d63a389a88b1a51
Rintaro Itokawa <me@re-taro.dev>
2022-09-24 21:54:28 +0900

    feat: Move button model to near the button component file

---
+++ src/components/atoms/button/type/model.ts
@@ -0,0 +1,11 @@
+import type { CSSInterpolation } from "@emotion/serialize";
+import type { PropsWithChildren } from "react";
+
+export type ButtonProperties<T extends AnyComponent> = PropsWithChildren<
+  PropertiesOf<T> & {
+  boxStyles?: CSSInterpolation;
+  textStyles?: CSSInterpolation;
+  ref?: never;
+  as?: T;
+}
+  >;
