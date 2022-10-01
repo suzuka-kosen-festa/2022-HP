22ecf806b27cb3c711a83fb8cff91c1c18cbc7db
sansai-SNCT <syunzo1115@icloud.com>
2022-10-02 01:15:35 +0900

    [update] FooterBoxを修正

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -3,7 +3,7 @@ import tw from "twin.macro";
 import type { FooterProperties } from "../../../models";
 import { AnchorText } from "../../atoms/anchor-text";
 
-const FooterBox = tw.footer`bg-black text-center leading-[calc(12px + 4 * ((100vw - 378px) / 1134))] py-[calc(100vw / 63)]`;
+const FooterBox = tw.footer`flex flex-col justify-center items-center`;
 
 const Footer: FC<FooterProperties> = ({ link, children, ...rest }) => (
   <FooterBox role="contentinfo" {...rest}>
