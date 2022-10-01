4703b71a9a54c110c9726cce01abd5989cbe2d8d
Rintaro Itokawa <me@re-taro.dev>
2022-10-01 23:06:27 +0900

    feat: Update festa section

--- src/components/organisms/festa-section/type/model.ts
+++ src/components/organisms/festa-section/type/model.ts
@@ -2,5 +2,6 @@ import type { ComponentProps, ReactHTML } from "react";
 
 export type FestaSectionProperties = Omit<ComponentProps<ReactHTML["section"]>, "children"> & {
   title: string;
+  subtitle: string;
   children: string;
 };
