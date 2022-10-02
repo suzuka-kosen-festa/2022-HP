62d09a277bc0ce55e878975a5c364308c316c8f7
sansai-SNCT <syunzo1115@icloud.com>
2022-10-02 21:49:27 +0900

    [update] update contact-section

--- src/components/organisms/contact-section/contact-section.stories.tsx
+++ src/components/organisms/contact-section/contact-section.stories.tsx
@@ -8,11 +8,37 @@ const data = {
   buttonText: "お問い合わせ",
   children: "第57回鈴鹿高専祭に関するお問い合わせは下のボタンからお願いいたします。(google formsが開きます)",
   link: "#",
-  title: "お問い合わせ",
+  title: "Contact us",
 };
 
 export default {
   args: { buttonText: data.buttonText, children: data.children, link: data.link, title: data.title },
+  argTypes: {
+    buttonText: {
+      description: "The text for button in section",
+      control: {
+        type: "text",
+      },
+    },
+    children: {
+      description: "Value of main sentence in section",
+      control: {
+        type: "text",
+      },
+    },
+    link: {
+      description: "Href to GoogleForms",
+      control: {
+        type: "text",
+      },
+    },
+    title: {
+      description: "Title of section",
+      control: {
+        type: "text",
+      },
+    },
+  },
   component: ContactSection,
 } as ComponentMeta<T>;
 
