ee0f9c6fa727de4af4dd73f1d0fade9c2c441b99
Rintaro Itokawa <me@re-taro.dev>
2022-10-07 00:45:50 +0900

    feat: Create ticket section

---
+++ src/components/organisms/ticket-section/ticket-section.stories.tsx
@@ -0,0 +1,46 @@
+import type { ComponentStoryObj, ComponentMeta } from "@storybook/react";
+import { ComponentPropsWithoutRef } from 'react';
+import { TicketSection } from ".";
+
+type T = typeof TicketSection;
+type Story = ComponentStoryObj<T>;
+type Meta = ComponentMeta<T>;
+
+const args: ComponentPropsWithoutRef<T> = {
+  title: "Ticket",
+  children: "感染拡大防止のための入場制限により、ご入場いただける方を\n本校学生とその家族・学生の友人各2名・本校の受験を考える中学生とその保護者1名・本校OB、OGの皆様・ご協賛いただいた企業の方に限定させていただいております。\n\nまた、中学生の皆様向けに 中学生受験相談室 というイベントを実施いたしますので、ぜひご参加ください。\n日時：10/30（日)  10:30~12:00、14:00~15:30\n場所：マルチメディア棟1階 図書館",
+  anchorData: [
+    {
+      context: "中学生とその保護者の皆様の入場受付はこちら",
+      link: "#",
+      key: "JHS"
+    },
+    {
+      context: "OB・OGの皆様の入場受付はこちら",
+      link: "#",
+      key: "OB"
+    }
+  ]
+};
+
+export default {
+  args,
+  argTypes: {
+    title: { description: "Title of section", control: { type: "text" } },
+    children: {
+      description: "Value of main sentence in section",
+      control: {
+        type: "text",
+      },
+    },
+    anchorData: {
+      description: "Data of admission reception",
+      control: {
+        type: "array"
+      }
+    }
+  },
+  component: TicketSection,
+} as Meta;
+
+export const Default: Story = {};
