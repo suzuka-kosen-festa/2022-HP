06fc7c5d3e672b9c44a083b0d609570ea1da6252
Rintaro Itokawa <me@re-taro.dev>
2022-10-07 15:02:31 +0900

    feat: Create ticket section (#263)
    * chore: Increased reliance on SNS data and sparse components.
    
    * feat: Create ticket section
    
    * chore: コードを整形
    
    * chore: Format code
    
    Co-authored-by: re-taro <re-taro@users.noreply.github.com>

--- src/components/organisms/sns-section/index.tsx
+++ src/components/organisms/sns-section/index.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 import tw from "twin.macro";
-import type { SnsSectionProperties, SocialData } from "./type/model";
+import type { SnsSectionProperties } from "./type/model";
 import { Heading } from "../../atoms/heading";
 import { Text } from "../../atoms/text";
 import Icon from "../../atoms/icon";
@@ -9,41 +9,14 @@ const SnsSectionContainer = tw.section`flex flex-col justify-center items-center
 
 const Social = tw.div`flex space-x-8 sm:space-x-12 justify-center items-center`;
 
-const datas: Array<SocialData> = [
-  {
-    icon: "ci:twitter",
-    key: "Twitter",
-    link: "https://twitter.com/KOSENFESTA",
-    style: tw`text-social-twitter hover:text-social-twitter-reaction`,
-  },
-  {
-    icon: "ci:instagram",
-    key: "Instagram",
-    link: "https://www.instagram.com/kosenfesta/",
-    style: tw`text-white bg-social-instagram hover:bg-social-instagram-reaction rounded-md sm:rounded-2xl`,
-  },
-  {
-    icon: "ci:youtube",
-    key: "YouTube",
-    link: "https://www.youtube.com/channel/UCV4zdsdagV2N3juHRz2dqQQ",
-    style: tw`text-social-youtube hover:text-social-youtube-reaction`,
-  },
-  {
-    icon: "ci:spotify",
-    key: "Spotify",
-    link: "https://open.spotify.com/show/4n0D0dsaFaPbJTz7tGpfTR?si=8yZ1H8LzTM-ZJPyYN6CU-A",
-    style: tw`text-social-spotify hover:text-social-spotify-reaction`,
-  },
-];
-
-const SnsSection: FC<SnsSectionProperties> = ({ title, children, ...rest }) => (
+const SnsSection: FC<SnsSectionProperties> = ({ title, children, socialData, ...rest }) => (
   <SnsSectionContainer aria-label="SNS" {...rest}>
     <Heading css={tw`mb-4 sm:mb-8`} colorKey="blue">
       {title}
     </Heading>
     <Text css={tw`whitespace-pre-wrap mb-6 sm:mb-16`}>{children}</Text>
     <Social>
-      {datas.map(data => (
+      {socialData.map(data => (
         <Icon
           as="a"
           css={tw`w-10 h-10 sm:(w-20 h-20) cursor-pointer`}
--- src/components/organisms/sns-section/sns-section.stories.tsx
+++ src/components/organisms/sns-section/sns-section.stories.tsx
@@ -1,5 +1,6 @@
 import type { ComponentStoryObj, ComponentMeta } from "@storybook/react";
 import type { ComponentPropsWithoutRef } from "react";
+import tw from "twin.macro";
 import { SnsSection } from ".";
 
 type T = typeof SnsSection;
@@ -9,6 +10,32 @@ type Meta = ComponentMeta<T>;
 const args: ComponentPropsWithoutRef<T> = {
   title: "SNS",
   children: "今年の鈴鹿高専祭では、史上初となるラジオの配信を行っています。\nご視聴はYouTube、Spotifyから！",
+  socialData: [
+    {
+      icon: "ci:twitter",
+      key: "Twitter",
+      link: "https://twitter.com/KOSENFESTA",
+      style: tw`text-social-twitter hover:text-social-twitter-reaction`,
+    },
+    {
+      icon: "ci:instagram",
+      key: "Instagram",
+      link: "https://www.instagram.com/kosenfesta/",
+      style: tw`text-white bg-social-instagram hover:bg-social-instagram-reaction rounded-md sm:rounded-2xl`,
+    },
+    {
+      icon: "ci:youtube",
+      key: "YouTube",
+      link: "https://www.youtube.com/channel/UCV4zdsdagV2N3juHRz2dqQQ",
+      style: tw`text-social-youtube hover:text-social-youtube-reaction`,
+    },
+    {
+      icon: "ci:spotify",
+      key: "Spotify",
+      link: "https://open.spotify.com/show/4n0D0dsaFaPbJTz7tGpfTR?si=8yZ1H8LzTM-ZJPyYN6CU-A",
+      style: tw`text-social-spotify hover:text-social-spotify-reaction`,
+    },
+  ],
 };
 
 export default {
@@ -19,6 +46,12 @@ export default {
       description: "Value of main sentence in section",
       control: {
         type: "text",
+      },
+    },
+    socialData: {
+      description: "Data of social media",
+      control: {
+        type: "array",
       },
     },
   },
