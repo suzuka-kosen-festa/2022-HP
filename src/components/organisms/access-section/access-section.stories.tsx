03d87e1a97d37421486b162fb1370a322d233d01
Rintaro Itokawa <me@re-taro.dev>
2022-10-13 21:20:47 +0900

    feat: Update access section
    Add parking map

--- src/components/organisms/access-section/access-section.stories.tsx
+++ src/components/organisms/access-section/access-section.stories.tsx
@@ -7,12 +7,14 @@ type Story = ComponentStoryObj<T>;
 type Meta = ComponentMeta<T>;
 
 const args: ComponentPropsWithoutRef<T> = {
+  attention: `駐車場はスペースに限りがございますのでご了承ください。\n30日(日)は鈴鹿医療科学大学 白子キャンパス 様\n31日(月)はMEGAドン・キホーテUNY鈴鹿店 様の屋上駐車場を\nご利用ください。`,
   center: {
     lat: 34.851_671_242_413_17,
     lng: 136.581_323_097_843_55,
   },
-  children: `510-0294 三重県鈴鹿市白子町\n近鉄白子駅から徒歩約30分\n三重交通バス東旭が丘3丁目から徒歩約7分`,
+  info: `510-0294 三重県鈴鹿市白子町\n近鉄白子駅から徒歩約30分\n三重交通バス東旭が丘3丁目から徒歩約7分`,
   label: "鈴鹿工業高等専門学校",
+  subtitle: "お車でお越しの方へ",
   title: "Access",
   zoom: 15,
 };
@@ -20,20 +22,32 @@ const args: ComponentPropsWithoutRef<T> = {
 export default {
   args,
   argTypes: {
+    attention: {
+      description: "Attention context",
+      control: {
+        type: "text",
+      },
+    },
     center: {
       description: "Center of map",
       control: {
         type: "object",
       },
     },
-    children: {
-      description: "Value of main sentence in section",
+    info: {
+      description: "Address of NIT Suzuka",
       control: {
         type: "text",
       },
     },
     label: {
       description: "Label of map",
+      control: {
+        type: "text",
+      },
+    },
+    subtitle: {
+      description: "Sub title for attention",
       control: {
         type: "text",
       },
--- src/components/organisms/access-section/types/model.ts
+++ src/components/organisms/access-section/types/model.ts
@@ -1,11 +1,13 @@
 import type { ComponentProps, ReactHTML } from "react";
 
 export type MapSectionProperties = ComponentProps<ReactHTML["section"]> & {
+  attention: string;
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   center: google.maps.LatLngLiteral;
   zoom: number;
   title: string;
   label: string;
-  children: string;
+  info: string;
+  subtitle: string;
 };
