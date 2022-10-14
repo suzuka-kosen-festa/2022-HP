ef1da23a7478a0b3fa06472b9070d8aab37c608b
Rintaro Itokawa <me@re-taro.dev>
2022-10-14 20:38:46 +0900

    chore: Update tewst case
    forgot about aria role and atomic design

--- src/components/organisms/bazaar-section/bazaar-section.test.tsx
+++ src/components/organisms/bazaar-section/bazaar-section.test.tsx
@@ -5,7 +5,19 @@ import * as stories from "./bazaar-section.stories";
 
 const { Default } = composeStories(stories);
 
+const options = {
+  name: "bazaar",
+};
+
 describe("(components) organisms/bazaar-section", () => {
+  test("to be organisms", () => {
+    const { container } = render(<Default />);
+    expect(container).toBeOrganism();
+  });
+  test("to be [role=region]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("region", options));
+  });
   test("take snap shot", () => {
     const { container } = render(<Default />);
     expect(container).toMatchSnapshot();
