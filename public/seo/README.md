f724948bb24ea5b727ec68c4ae6b52bc5ac1b2e4
Rintaro Itokawa <me@re-taro.dev>
2022-10-19 02:21:58 +0900

    chore: Format code

--- public/seo/README.md
+++ public/seo/README.md
@@ -22,4 +22,4 @@ Insert the following code in the `head` section of your pages:
     <meta name="msapplication-config" content="/seo/browserconfig.xml">
     <meta name="theme-color" content="#ffcb00">
 
-*Optional* - Check your favicon with the [favicon checker](https://realfavicongenerator.net/favicon_checker)
+_Optional_ - Check your favicon with the [favicon checker](https://realfavicongenerator.net/favicon_checker)
--- vite.config.ts
+++ vite.config.ts
@@ -70,20 +70,20 @@ export default defineConfig({
           {
             src: "/seo/android-chrome-192x192.png",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/png",
           },
           {
             src: "/seo/android-chrome-512x512.png",
             sizes: "512x512",
-            type: "image/png"
-          }
+            type: "image/png",
+          },
         ],
         theme_color: "#ffcb00",
         background_color: "#ffcb00",
         start_url: "https://reroad.site/",
         display: "standalone",
-        lang: "ja"
-      }
+        lang: "ja",
+      },
     }),
     splitVendorChunkPlugin(),
   ],
