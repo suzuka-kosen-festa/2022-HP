7e4dd373045a53da2e0fd49f6d96b76e1b9a4379
Rintaro Itokawa <me@re-taro.dev>
2022-10-20 01:11:53 +0900

    chore: Fix type in heading component

--- src/components/atoms/heading/types/model.ts
+++ src/components/atoms/heading/types/model.ts
@@ -2,5 +2,6 @@ import type { ComponentProps, ReactHTML } from "react";
 import type { ColorVariant } from "../../../../styles/type/model";
 
 export type HeadingProperties = ComponentProps<ReactHTML["h1"]> & {
+  children: string;
   colorKey: ColorVariant;
 };
