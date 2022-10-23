5df00abbe017c040507e243f92581513d66c0312
Rintaro Itokawa <me@re-taro.dev>
2022-10-24 00:15:43 +0900

    fix: Create custom hooks and fix scroll issue (#333)

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,11 @@ import type { FC } from "react";
 import { useRegisterSW } from "virtual:pwa-register/react";
 import { Route, Switch } from "wouter";
 import { TopPage, ComingSoonPage, SponsorPage } from "./pages";
+import { useScrollToTop } from "./hooks/useScrollToTop";
 
 const Router: FC = () => {
   useRegisterSW();
+  useScrollToTop();
   return (
     <Switch>
       <Route path="/" component={TopPage} />
