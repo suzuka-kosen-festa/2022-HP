cb199ef2d5aa52f5e48878210566ba110da60688
Rintaro Itokawa <me@re-taro.dev>
2022-10-23 16:40:21 +0900

    feat: Create bazaar card component (#320)
    * feat: Create bazaar card component
    
    * chore: Format code
    
    * chore: Update snapshot

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,8 +61,14 @@ module.exports = {
         "main-md": "8vw",
         main: "13.4vw",
       },
+      screens: {
+        xs: "360px",
+      },
       space: {
         2.25: "0.5625rem",
+      },
+      width: {
+        75: "18.75rem",
       },
     },
   },
