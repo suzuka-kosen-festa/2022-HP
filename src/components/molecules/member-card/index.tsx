7931fb9408a89bcfd7e95718b6c381626fa5d45b
Rintaro Itokawa <me@re-taro.dev>
2022-10-26 01:25:50 +0900

    chore: Fix styleing of member card

--- src/components/molecules/member-card/index.tsx
+++ src/components/molecules/member-card/index.tsx
@@ -2,15 +2,13 @@ import type { FC } from "react";
 import tw, {css} from "twin.macro";
 import type { MemberCardProperties } from "./types/model";
 
-const Image = tw.img`w-full h-full`
-
 const Position = tw.p`font-zen font-bold underline text-xs sm:text-2xl`
 
 const Name = tw.p`font-zen font-bold text-xs sm:text-2xl`
 
 const MemberCard: FC<MemberCardProperties> = ({ imagePath, name, position = "other" }) => (
   <figure>
-    <Image css={css`aspect-ratio: 3 / 2;`} src={imagePath} alt={name} width={450} height={300} loading="lazy" />
+    <img css={css`aspect-ratio: 3 / 2;`} src={imagePath} alt={name} width={450} height={300} loading="lazy" />
     <figcaption css={[tw`flex items-center justify-center`, position !== "other" ? tw`space-x-4 sm:space-x-8` : null]}>
       {position === "other" ? null : <Position>{position === "leader" ? "LEADER" : "SUB LEADER"}</Position>}
       <Name>{name}</Name>
