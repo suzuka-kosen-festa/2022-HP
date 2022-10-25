ef995f2fa7c5af4bfa28dbe3617826bce584fbb0
Rintaro Itokawa <me@re-taro.dev>
2022-10-26 02:08:29 +0900

    chore: Add button to link to top page

--- src/components/templates/member/index.tsx
+++ src/components/templates/member/index.tsx
@@ -1,8 +1,10 @@
 import type { FC } from "react";
 import tw from "twin.macro";
+import {Link} from "wouter";
 import type { MemberProperties } from "./types/model";
 import { Title } from "../../molecules/title";
 import { MemberCard } from "../../molecules/member-card";
+import {Button} from "../../atoms/button";
 
 const Main = tw.main`pt-1 pb-8 sm:(pt-6 pb-40)`;
 
@@ -21,11 +23,16 @@ const Member: FC<MemberProperties> = ({ rikuto, shoma, members }) => (
         <MemberCard imagePath={rikuto.imagePath} name={rikuto.name} position="leader" />
         <MemberCard imagePath={shoma.imagePath} name={shoma.name} position="sub" />
       </div>
-      <div css={tw`grid grid-cols-3 gap-2 lg:(grid-cols-5 gap-8)`}>
+      <div css={tw`grid grid-cols-3 gap-2 mb-8 sm:mb-40 lg:(grid-cols-5 gap-8)`}>
         {members.map(member => (
           <MemberCard imagePath={member.imagePath} name={member.name} key={member.name} />
         ))}
       </div>
+      <Link to="/">
+        <Button textStyles={tw`text-white`} colorKey="blue" as="a" role="button">
+          トップページに戻る
+        </Button>
+      </Link>
     </MemberBox>
   </Main>
 );
