042ec811efb972759f73030458542c619afc0da8
Rintaro Itokawa <me@re-taro.dev>
2022-10-26 01:26:27 +0900

    feat: Create member page template

---
+++ src/components/templates/member/member.stories.tsx
@@ -0,0 +1,88 @@
+import type { ComponentStoryObj, ComponentMeta } from "@storybook/react";
+import { ComponentPropsWithoutRef } from 'react';
+import { Member } from ".";
+
+type T = typeof Member;
+type Story = ComponentStoryObj<T>;
+type Meta = ComponentMeta<T>;
+
+const args: ComponentPropsWithoutRef<T> = {
+  rikuto: {
+    imagePath: "/statics/members/rikuto_kuwahara.webp",
+    name: "Rikuto Kuwahara"
+  },
+  shoma: {
+    imagePath: "/statics/members/shoma_kobayashi.webp",
+    name: "Shoma Kobayashi",
+    position: "sub"
+  },
+  members: [
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+    {
+      imagePath: "/statics/members/rintaro_itokawa.webp",
+      name: "Rintaro Itokawa"
+    },
+  ]
+};
+
+export default {
+  args,
+  argTypes: {
+    rikuto: {
+      description: "Rikuto Kuwahara's data",
+      control: {
+        type: "object"
+      }
+    },
+    shoma: {
+      description: "Shoma Kobayashi's data",
+      control: {
+        type: "object"
+      }
+    },
+    members: {
+      description: "Other member's data",
+      control: {
+        type: "array"
+      }
+    }
+  },
+  component: Member,
+} as Meta;
+
+export const Default: Story = {};
---
+++ src/components/templates/member/member.test.tsx
@@ -0,0 +1,25 @@
+import { composeStories } from "@storybook/testing-react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import * as stories from "./member.stories";
+
+const { Default } = composeStories(stories);
+
+const options = {
+  name: "member page"
+}
+
+describe("(components) templates/member", () => {
+  test("to be templates", () => {
+    const { container } = render(<Default />);
+    expect(container).toBeTemplate();
+  });
+  test("to be [role=main]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("main", options)).toBeInTheDocument();
+  });
+  test("take snap shot", () => {
+    const { container } = render(<Default />);
+    expect(container).toMatchSnapshot();
+  });
+});
