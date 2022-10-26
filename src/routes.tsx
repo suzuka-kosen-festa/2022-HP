425654b4f14031e60247e358c0387969f432e207
Rintaro Itokawa <me@re-taro.dev>
2022-10-27 00:50:58 +0900

    chore: Update rooting

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 import { useRegisterSW } from "virtual:pwa-register/react";
 import { Route, Switch } from "wouter";
-import { TopPage, SponsorPage, BazaarPage, MemberPage, NotFoundPage } from "./pages";
+import {TopPage, SponsorPage, BazaarPage, MemberPage, NotFoundPage, TimeTablePage} from "./pages";
 import { useScrollToTop } from "./hooks/useScrollToTop";
 import { useJwt } from "./hooks/useJwt";
 
@@ -14,6 +14,7 @@ const Router: FC = () => {
       <Route path="/" component={TopPage} />
       <Route path="/sponsors" component={SponsorPage} />
       <Route path="/bazaar" component={BazaarPage} />
+      <Route path="/timetable" component={TimeTablePage} />
       <Route path="/member" component={MemberPage} />
       <Route component={NotFoundPage} />
     </Switch>
