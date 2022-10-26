2b4c64b9566fc2010bc22a08ab4d9427b9522339
Rintaro Itokawa <me@re-taro.dev>
2022-10-26 21:26:10 +0900

    feat: Create time table template

---
+++ src/components/templates/time-table/index.tsx
@@ -0,0 +1,33 @@
+import type { FC } from "react";
+import tw from "twin.macro";
+import type { TimeTableProperties } from "./types/model";
+import {Title} from "../../molecules/title";
+import {Switching} from "../../atoms/switching";
+import {TimeTable as TT} from "../../organisms/time-table";
+
+const Main = tw.main`pt-1 pb-8 sm:(pt-6 pb-40)`;
+
+const TimetablePageBox = tw.div`flex flex-col justify-center items-center space-y-6 xl:space-y-32 3xl:space-y-48`;
+
+const buttonData = [
+  {
+    buttonText: "10.30",
+    query: "30",
+  },
+  {
+    buttonText: "10.31",
+    query: "31",
+  },
+];
+
+const TimeTable: FC<TimeTableProperties> = ({ events, intervals }) => (
+  <Main aria-label="Time table">
+    <Title colorKey="yellow">Time schedule</Title>
+    <TimetablePageBox>
+      <Switching buttonData={buttonData} color="yellow" />
+      <TT events={events} intervals={intervals} />
+    </TimetablePageBox>
+  </Main>
+);
+
+export { TimeTable };
