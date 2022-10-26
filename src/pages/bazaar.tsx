6712a8f88c94e0cfaf4d8ed1b8d7cde2f559e1e8
Rintaro Itokawa <me@re-taro.dev>
2022-10-26 19:10:48 +0900

    feat: Update bazaar page

--- src/pages/bazaar.tsx
+++ src/pages/bazaar.tsx
@@ -1,7 +1,20 @@
 import type { FC } from "react";
+import useSWR from "swr";
+import { Redirect } from "wouter";
 import { Bazaar } from "../components/templates/bazaar";
+import { fetcher } from "../libs/axios";
+import type { BazaarWithId } from "../api/bazaar";
 
-// TODO: Fetch with swr
-const BazaarPage: FC = () => <Bazaar bazaars={[]} />;
+const BazaarPage: FC = () => {
+  const query = new URLSearchParams(window.location.search);
+  const isRecreation = query.get("select") === "recreation";
+  const { data, error } = useSWR<ReadonlyArray<BazaarWithId>>(
+    isRecreation ? "/bazaar/data/recreation" : "/bazaar/data/eating",
+    fetcher,
+  );
+  if (!data) return <p>Loading...</p>;
+  if (error) return <Redirect to="/error" />;
+  return <Bazaar bazaars={data} />;
+};
 
 export { BazaarPage };
